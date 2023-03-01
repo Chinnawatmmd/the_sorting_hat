81c7a2bac3ba94436d58a9c7947eb199e6601c93
shin <madmud_c@silpakorn.edu>
2023-03-01 22:48:12 +0700

    Add a reset button

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,7 +6,12 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton.large(
+        onPressed: (){},
+        backgroundColor: Colors.red,
+        child: Text('RESET'),
+      ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -25,6 +30,18 @@ class Home extends StatelessWidget {
                   child: Text('Insert a new student'),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('All student info'),
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/images/house/gryffindor.png', width: 100, height: 100,),
+                  Image.asset('assets/images/house/gryffindor.png', width: 100, height: 100,),
+                  Image.asset('assets/images/house/gryffindor.png', width: 100, height: 100,),
+                  Image.asset('assets/images/house/gryffindor.png', width: 100, height: 100,),
+                ],
+              )
             ],
           ),
         ],
