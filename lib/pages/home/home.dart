d02ea5d790ebd4d74933dccdfb8639a17c13b39f
shin <madmud_c@silpakorn.edu>
2023-03-01 21:54:58 +0700

    Add a sorting hat image and add insert button

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,7 +6,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Colors.black,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.network(
+              'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzc2YTc2NWFjNWRjMDAwM2Q5ZjE2YWM1YmQ4ZDIyYThiYjk3YWMwYiZjdD1z/l1KVaVCC1jrRoVlGE/giphy.gif', width: 500,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
 
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 8, left: 20, right: 20),
+                  child: Text('Insert a new student'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
